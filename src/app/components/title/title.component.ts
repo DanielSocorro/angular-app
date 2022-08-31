209b3a97b11f7a0bd4996b64b8f318b4bc7f51aa
Daniel Socorro <daniel.socorro.c@gmail.com>
2022-08-30 20:05:09 -0400

    adding component title

--- src/app/components/title/title.component.ts
+++ src/app/components/title/title.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-title',
   templateUrl: './title.component.html',
-  styleUrls: ['./title.component.scss']
+  styleUrls: ['./title.component.scss'],
 })
-export class TitleComponent implements OnInit {
+export class TitleComponent implements AfterViewInit {
+  @Input() titleText = 'this is my title';
+  showTitle: boolean;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.showTitle = true;
   }
 
+  ngAfterViewInit(): void {
+/*     const title =  document.getElementById('title');
+    alert('Initiated');
+    alert(title?.innerHTML); */
+
+  }
 }
